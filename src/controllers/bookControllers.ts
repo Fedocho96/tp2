276564df04337cf39bc961d4f45dc2e0f46d6338
Federico Rosales <fedocho.96@gmail.com>
2025-06-10 19:47:04 -0300

    controllers and router done

--- src/controllers/bookControllers.ts
+++ src/controllers/bookControllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Book } from "../models/bookModel";
+import bookInterface from "../interfaces/Book";
 import createResponseToClient from "../utils/createResToClient";
 
 const getAllBooks = async (req:Request, res:Response) => {
@@ -11,4 +12,63 @@ const getAllBooks = async (req:Request, res:Response) => {
   }
 };
 
-export { getAllBooks };
+const getBookById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const book = await Book.findById(id);
+    if (!book) {
+       res.status(404).json(createResponseToClient(false, 404, "Book not found"));
+    }
+    res.json(createResponseToClient(true, 200, "Book retrieved", book));
+  } catch (error: any) {
+    res.status(500).json(createResponseToClient(false, 500, error.message));
+  }
+};
+
+const createBook = async (req: Request, res: Response) => {
+  const { title, author, publishedYear, genre } = req.body as bookInterface;
+
+  if (!title || !author) {
+     res
+      .status(400)
+      .json(createResponseToClient(false, 400, "Title and author are required"));
+  }
+
+  try {
+    const newBook = new Book({ title, author, publishedYear, genre });
+    await newBook.save();
+    res.status(201).json(createResponseToClient(true, 201, "Book created", newBook));
+  } catch (error: any) {
+    res.status(500).json(createResponseToClient(false, 500, error.message));
+  }
+};
+
+const updateBook = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const updates: Partial<bookInterface> = req.body;
+
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(id, updates, { new: true });
+    if (!updatedBook) {
+       res.status(404).json(createResponseToClient(false, 404, "Book not found"));
+    }
+    res.json(createResponseToClient(true, 200, "Book updated", updatedBook));
+  } catch (error: any) {
+    res.status(500).json(createResponseToClient(false, 500, error.message));
+  }
+};
+
+const deleteBook = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const deletedBook = await Book.findByIdAndDelete(id);
+    if (!deletedBook) {
+       res.status(404).json(createResponseToClient(false, 404, "Book not found"));
+    }
+    res.json(createResponseToClient(true, 200, "Book deleted", deletedBook));
+  } catch (error: any) {
+    res.status(500).json(createResponseToClient(false, 500, error.message));
+  }
+};
+
+export { getAllBooks, getBookById, createBook, updateBook, deleteBook };
--- src/routes/bookRouter.ts
+++ src/routes/bookRouter.ts
@@ -1,8 +1,5 @@
 import { Router } from "express";
-import { Book } from "../models/bookModel";
-import bookInterface from "../interfaces/Book";
-import createResponseToClient from "../utils/createResToClient";
-import { getAllBooks } from "../controllers/bookControllers";
+import { getAllBooks, getBookById, createBook, updateBook, deleteBook } from "../controllers/bookControllers";
 
 const gameRouter = Router();
 
@@ -10,66 +7,15 @@ const gameRouter = Router();
 gameRouter.get("/api/books", getAllBooks )
 
 // GET BOOK BY ID
-gameRouter.get("/api/books/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const book = await Book.findById(id);
-    if (!book) {
-       res.status(404).json(createResponseToClient(false, 404, "Book not found"));
-    }
-    res.json(createResponseToClient(true, 200, "Book retrieved", book));
-  } catch (error: any) {
-    res.status(500).json(createResponseToClient(false, 500, error.message));
-  }
-});
+gameRouter.get("/api/books/:id", getBookById)
 
 // CREATE NEW BOOK
-gameRouter.post("/api/books", async (req, res) => {
-  const { title, author, publishedYear, genre } = req.body as bookInterface;
-
-  if (!title || !author) {
-     res
-      .status(400)
-      .json(createResponseToClient(false, 400, "Title and author are required"));
-  }
-
-  try {
-    const newBook = new Book({ title, author, publishedYear, genre });
-    await newBook.save();
-    res.status(201).json(createResponseToClient(true, 201, "Book created", newBook));
-  } catch (error: any) {
-    res.status(500).json(createResponseToClient(false, 500, error.message));
-  }
-});
+gameRouter.post("/api/books", createBook)
 
 // UPDATE BOOK
-gameRouter.patch("/api/books/:id", async (req, res) => {
-  const { id } = req.params;
-  const updates: Partial<bookInterface> = req.body;
-
-  try {
-    const updatedBook = await Book.findByIdAndUpdate(id, updates, { new: true });
-    if (!updatedBook) {
-       res.status(404).json(createResponseToClient(false, 404, "Book not found"));
-    }
-    res.json(createResponseToClient(true, 200, "Book updated", updatedBook));
-  } catch (error: any) {
-    res.status(500).json(createResponseToClient(false, 500, error.message));
-  }
-});
+gameRouter.patch("/api/books/:id", updateBook)
 
 // DELETE BOOK
-gameRouter.delete("/api/books/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const deletedBook = await Book.findByIdAndDelete(id);
-    if (!deletedBook) {
-       res.status(404).json(createResponseToClient(false, 404, "Book not found"));
-    }
-    res.json(createResponseToClient(true, 200, "Book deleted", deletedBook));
-  } catch (error: any) {
-    res.status(500).json(createResponseToClient(false, 500, error.message));
-  }
-});
+gameRouter.delete("/api/books/:id", deleteBook)
 
 export default gameRouter;
